ad342d519166212fd7e7184685e04ae5075b41d1
KIRIZAMUSHAGA <Kirizamushaga01@gmail.com>
2025-10-03 22:32:53 +0100

    Add HomePage component

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to Smartohada</h1>
+            <p>This is the homepage of the Smartohada application.</p>
+        </div>
+    );
+};
+
+export default HomePage;
